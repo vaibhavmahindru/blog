e4b75107bd694eda96eafbc7a0902c377bc155f0
vaibhavmahindru <500067728@stu.upes.ac.in>
2020-05-24 15:15:17 +0530

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const router = require('./route/first');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded ( { extended:false } ) );//body parser
 
 app.use(express.static(path.join(__dirname,'public')));//server static files
