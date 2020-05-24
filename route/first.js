74b56b148f80147f255da3c320c8b5905e71fd8e
vaibhavmahindru <500067728@stu.upes.ac.in>
2020-05-24 19:01:57 +0530

    final

--- route/first.js
+++ route/first.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');  
 const Article = require('./../models/articles')
-const url = process.env.MONGODB_URI || 'mongodb://localhost/blog'
+const url = process.env.MONGODB_URI || 'mongodb://localhost/blog' //url for the database connection
 
 mongoose.connect( url , { 
     useNewUrlParser: true,
@@ -13,16 +13,18 @@ router.get('/', (req,res,next)=>{
     res.render(__dirname + '/views/first.ejs');
 });
 
-
+//read router
 router.get('/read', async (req,res,next)=>{
     const article = await Article.find().sort({createdAt: 'desc'})
     res.render(__dirname + '/views/read.ejs', { articles : article });
 });
 
+//write router to direct the user to the write page
 router.get('/write', (req,res,next)=>{
     res.render(__dirname + '/views/write.ejs');
 });
 
+//router to show any particular blog by id
 router.get('/:id',  (req, res) => {
     
     Article.findOne({_id : req.params.id}, function(err,obj) { 
@@ -31,6 +33,7 @@ router.get('/:id',  (req, res) => {
    
 })
 
+//submit router
 router.post('/submit', async (req,res)=>{
     let article = new Article({
         title: req.body.title,
@@ -42,7 +45,7 @@ router.post('/submit', async (req,res)=>{
        article =  await article.save(),
        res.render(__dirname + '/views/show.ejs', {article : article});
     }catch(e){
-        res.render(__dirname + '/views/write.ejs',{ article : article })
+        res.render(__dirname + '/views/write.ejs',{ article : article })//redirect the user to write page with the previous content already loaded.
     }
 })
 
